08031cf3b366d8070331bd311100298ec1082aea
shivam <shivambhandari378@gmail.com>
2021-05-19 04:24:00 +0530

    first

--- module1/JS_concepts/scope.js
+++ module1/JS_concepts/scope.js
@@ -1,1 +1,24 @@
-//let 
+//let is  block scoped
+//const is block scoped
+
+//var => function scope
+
+var a =20
+
+console.log(a)
+
+if(true){
+    var a= 50
+    console.log(a)
+}
+
+console.log(a)
+
+
+function callme(){
+    var a= 100;
+    console.log("inside call me")
+    console.log(a)
+}
+callme();
+console.log(a);
