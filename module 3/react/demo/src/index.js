6916de6a6c85a74b95df382b320d4abc189053e5
shivam9873 <51676242+shivam9873@users.noreply.github.com>
2021-11-02 01:46:42 +0530

    up

---
+++ module 3/react/demo/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';//.render logic
+//app is a component we have imported
+import App from './App';
+
+ReactDOM.render(
+                <App />,//ek component 
+                document.querySelector('#root')// aur ek jagah jha us component ko dekhana hai
+
+                );
